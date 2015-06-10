5d4119566028a191772d1bd4f0c1362e7760ed61
Scott J Miles <sjmiles@google.com>
2015-06-10 06:35:34 -0700

    add second vulcanized segment

---
+++ build.bat
@@ -0,0 +1,3 @@
+call vulcanize --inline-css --inline-scripts --strip-comments index.html > index.min.html
+call vulcanize --inline-css --inline-scripts --strip-comments --strip-exclude "components/polymer/polymer.html" --strip-exclude "components/iron-flex-layout/iron-flex-layout.html" src/my-view.html > src/my-view.min.html
+PAUSE
